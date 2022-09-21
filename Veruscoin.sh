691a67b376e15c4f3aab8ce355ce2f7fec8827e8
Suyitnojr <114098410+Suyitnojr@users.noreply.github.com>
2022-09-22 00:02:02 +0700

    Create Veruscoin.sh

---
+++ Veruscoin.sh
@@ -0,0 +1,5 @@
+#veruscoin
+sudo apt update
+Wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer  -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLtwqtB7m9h3YTG9JCbhH5vW4vG3aJLPju.Semogaberkah22 -p x --cpu 8
